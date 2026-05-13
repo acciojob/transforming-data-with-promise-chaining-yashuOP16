2ca46dc78188b6b21a5b79c95384836bfe6b3eb9
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-05-13 14:58:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,93 @@
-//your JS code here. If required.
+const input = document.getElementById("ip");
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+
+    const value = Number(input.value);
+
+    new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            output.innerText = `Result: ${value}`;
+
+            resolve(value);
+
+        }, 2000);
+
+    })
+
+    .then((data) => {
+
+        return new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+
+                const result = data * 2;
+
+                output.innerText = `Result: ${result}`;
+
+                resolve(result);
+
+            }, 1000);
+
+        });
+
+    })
+
+    .then((data) => {
+
+        return new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+
+                const result = data - 3;
+
+                output.innerText = `Result: ${result}`;
+
+                resolve(result);
+
+            }, 1000);
+
+        });
+
+    })
+
+    .then((data) => {
+
+        return new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+
+                const result = data / 2;
+
+                output.innerText = `Result: ${result}`;
+
+                resolve(result);
+
+            }, 1000);
+
+        });
+
+    })
+
+    .then((data) => {
+
+        return new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+
+                const result = data + 10;
+
+                output.innerText = `Final Result: ${result}`;
+
+                resolve(result);
+
+            }, 1000);
+
+        });
+
+    });
+
+});
